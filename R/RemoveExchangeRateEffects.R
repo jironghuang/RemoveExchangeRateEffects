30d421962fec3646e9c4808371464bcf77e2238d
HUANG JIRONG <jironghuang88@gmail.com>
2018-10-28 11:38:26 +0800

    updated documentation

---
+++ R/RemoveExchangeRateEffects.R
@@ -0,0 +1,13 @@
+#' RemoveExchangeRateEffects: A package for decomposing exchange rates
+#'
+#' The RemoveExchangeRateEffects package allows you decompose 1 instrument position in SGD (column value) - from the perspective of someone staying in Singapore - into local static value (i.e if I keep the exchange rate constant at the start of the period) and the residual exchange rate impact.
+#'
+#' @section exchange_rate_decomposition class:
+#' The class aids you in stripping out exchange effects (e.g. investment portfolio, revenue)
+#' 
+#' @section multiple_exchange_rate_decomposition class:
+#' Works like previous class. But you can use this on multiple instruments at once
+#'
+#' @docType package
+#' @name RemoveExchangeRateEffects
+NULL
